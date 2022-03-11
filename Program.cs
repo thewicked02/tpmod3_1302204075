5c0a528e4b81371a962854df9517ce57e7a0f7d2
rodo <rodonatanael02@gmail.com>
2022-03-11 13:57:00 +0700

    menambahkan implementasi dengan state-based construction

--- Program.cs
+++ Program.cs
@@ -42,3 +42,44 @@ namespace tpmodul3
     }
 }
 
+
+namespace tpmodul3
+{
+    internal class KodePos
+    {
+        public static void getKodePos(string kel, string kode)
+        {
+            Console.WriteLine(kel + " " + kode);
+        }
+
+    }
+}
+
+
+namespace tpmodul3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            KodePos KodePos = new KodePos();
+            KodePos.getKodePos("Kelurahan", "Kode Pos");
+            KodePos.getKodePos("Batununggal", "40266");
+            KodePos.getKodePos("Kujangsari", "40287");
+            KodePos.getKodePos("Mengger", "40267");
+            KodePos.getKodePos("Wates", "40256");
+            KodePos.getKodePos("Cijaura", "40287");
+            KodePos.getKodePos("Jatisari", "40286");
+            KodePos.getKodePos("Margasari", "40286");
+            KodePos.getKodePos("Sekejati", "40286");
+            KodePos.getKodePos("Kebonwaru", "40272");
+            KodePos.getKodePos("Maleer", "40274");
+            KodePos.getKodePos("Samoja", "40273");
+
+            DoorMachine.Mulai();
+        }
+    }
+
+
+}
