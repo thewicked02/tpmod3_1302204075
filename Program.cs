c192c32b0b670cfb99b34de3c3c88e8a8967385b
rodo <rodonatanael02@gmail.com>
2022-03-11 13:54:48 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3
+{
+    internal class DoorMachine
+    {
+        enum State { terkunci, terbuka, exit };
+
+        public static void Mulai()
+        {
+            Console.WriteLine("Masukan State");
+            State state = State.terkunci;
+            while (state != State.exit)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "terkunci":
+                        Console.WriteLine("Pintu terkunci");
+                        Console.WriteLine();
+                        break;
+                    case "terbuka":
+                        Console.WriteLine("Pintu tidak terkunci");
+                        Console.WriteLine();
+                        break;
+                    case "exit":
+                        state = State.exit;
+                        break;
+                    default:
+                        Console.WriteLine("Masukan ulang State");
+                        break;
+
+
+                }
+            }
+
+        }
+    }
+}
+
